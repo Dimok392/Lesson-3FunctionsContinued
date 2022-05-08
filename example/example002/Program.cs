eb2ccfbb91e5988dc56fd4b259e3407a430ce672
Пётр <dimok392@mail.ru>
2022-05-08 16:46:47 +0300

    example/example002

---
+++ example/example002/Program.cs
@@ -0,0 +1,25 @@
+﻿//Цикл FOR
+//
+string Method4(int count, string text)
+{
+    string result = String.Empty;//пустая строка 
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(4, "text");
+System.Console.WriteLine(res);
+
+// цикл в цикле
+//вывод таблицы умножения
+for (int i = 2; i <= 10 ; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        System.Console.WriteLine($"{i} * {j} = {i*j}");
+    }
+    System.Console.WriteLine();
+}
